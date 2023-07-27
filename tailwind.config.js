4a3cda7629c18020403fa8e41a7df6f42ff76253
sumon paul <sumpaul100@gmail.com>
2023-07-27 21:41:02 +0600

    B8A3-fruit-brust project assign ment 3

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primarycolor: "#F85559",
+      },
+    },
+  },
+  plugins: [],
+};
